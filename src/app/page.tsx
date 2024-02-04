2363c86acee372853d8e7e3b3accc5d7dc2a4a79
sanjib <sanjibdey.dey4@gmail.com>
2024-02-04 21:52:47 +0530

    style: simplify homepage welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,20 @@
-"use client";
-import HighlightedText from "./components/HighlightedText";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <section className="portfolio-scope-explanation w-full h-full p-20">
-        <h1 className="w-full lg:w-1/3 text-6xl mb-8 font-semibold">
-          presenting <HighlightedText>version 2</HighlightedText> of my
-          portfolio
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <section className="portfolio-scope-explanation w-full h-full p-8 lg:p-20">
+        <h1 className="w-full lg:w-1/2 text-5xl mb-1 font-semibold">
+          Hello, seeker.
         </h1>
 
-        <div className="explanation w-full lg:w-1/2 mb-8">
-          <p>
-            the idea is to{" "}
-            <HighlightedText>explore and implement</HighlightedText>{" "}
-            fundamentals, different tools and technologies, and maintain the
-            portfolio as a journal.
-          </p>
-          <p>tracking everything from findings to failures.</p>
-        </div>
+        <h1 className="w-full lg:w-1/2 text-5xl mb-8 font-semibold">
+          Welcome to my programming journal.
+        </h1>
 
         <div className="current-research__tracker">
           <h4 className="flex items-center gap-1 mb-1">
             <span className="current-experiment-pulse-indicator"></span>
-            <span>currently experimenting</span>
+            <span>Currently exploring: SVG paths</span>
           </h4>
-
-          <p className="current-research__domain mb-1">
-            [domain]: creative coding{" "}
-          </p>
-
-          <p className="current-research__topic mb-1">
-            [topic]: SVGs (specifically curves){" "}
-          </p>
-
-          <p className="current-research-topic__brief">
-            [brief]: generating custom highlighter SVG for specific texts
-          </p>
         </div>
       </section>
     </main>
